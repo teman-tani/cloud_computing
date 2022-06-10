d317e8abafb5245feb4e0a619e3d0132b382b9d1
slstyrn21 <slstyrn21@gmail.com>
2022-06-10 22:36:41 +0800

    ubah semua

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import (Flask, render_template, request)
+from flask import (Flask,  render_template, request,jsonify
+                   )
 from keras.models import load_model
-from keras.preprocessing import image
 import tensorflow as tf
 import numpy as np
 import os
 import pickle
 import pandas as pd
-from flask import jsonify
 
 app = Flask(__name__)
 
@@ -21,9 +20,29 @@ model = load_model('model_ml/my_model.h5')
 def index():
     return render_template('sendImage.html')
 
-df = pickle.load(open("model_ml/recommend_data.pkl", "rb"))
+
+
+def cleaning_data(data):
+
+    data = data.to_dict(orient='dict')
+    pesticide_name = list(data['nama'].values())
+    pesticide_image = list(data['image_src'].values())
+    pesticide_link = list(data['product_link'].values())
+    pesticide_tempat = list(data['tempat'].values())
+    # pesticede_image = data.image_src.to_json()
+
+    clean_data = []
+    for i in range(len(data)) :
+        temporary_data = {}
+        temporary_data['nama'] = pesticide_name[i]
+        temporary_data['image_src'] = pesticide_image[i]
+        temporary_data['product_link'] = pesticide_link[i]
+        temporary_data['tempat'] = pesticide_tempat[i]
+        clean_data.append(temporary_data)
+    return clean_data
+
+df = pickle.load(open("model_ml/recommend_data (1).pkl", "rb"))
 similarity = pickle.load(open("model_ml/similarity.pkl", "rb"))
-# list_movie = np.array(df["cleaned_desc"])
 
 @app.route('/recommend/<pesticide_name>')
 def recommend_pestisida_name(pesticide_name):
@@ -36,19 +55,19 @@ def recommend_pestisida_name(pesticide_name):
     # Scores of the 5 most similar name
     sig = sorted(sig, key=lambda x: x[1], reverse=True)
 
-    sig = sig[1:10]  # Book indicies
+    sig = sig[1:10]  # pesticides indicies
     pesticide_indices = [i[0] for i in sig]
 
     #   # Top 5 pesticide recommendation
     rec = df[['nama', 'kegunaan', 'tempat',
-              'berat', 'image-src', 'product_link']].iloc[pesticide_indices]
+              'berat', 'image_src', 'product_link']].iloc[pesticide_indices]
 
-    # response_json = {
-        #     recommendations : rec
-        # }
-    # return jsonify(response_json)
+    cleaning = cleaning_data(rec)
 
-    return render_template('resultRecommend.html', recommend = rec)
+    return jsonify(recommendations = cleaning)
+
+    # return render_template('resultRecommend.html', recommend = rec)
+
 
 def recommend_pestisida(disease):
     indices = pd.Series(df.index, index=df['nama'])
@@ -75,26 +94,28 @@ def recommend_pestisida(disease):
     sig = sig[1:10]  # Book indicies
     pesticide_indices = [i[0] for i in sig]
 
-    #   # Top 5 pesticide recommendation
+    # Top 5 pesticide recommendation
     rec = df[['nama', 'kegunaan', 'tempat',
-              'berat', 'image-src', 'product_link']].iloc[pesticide_indices]
+              'berat', 'image_src', 'product_link']].iloc[pesticide_indices]
 
+    cleaning = cleaning_data(rec)
     # print(rec)
-    return rec
+    return cleaning
 
 @app.route('/resultmodel', methods=['POST'])
 def result_model():
         
         images = request.files['img']
 
+        # path file
+        path = os.path.join(app.config['UPLOAD_PATH'], images.filename)
+
         # save file
         if images.filename != '':
-            images.save(os.path.join(
-                app.config['UPLOAD_PATH'], images.filename))
+            images.save(path)
 
         img = tf.keras.preprocessing.image.load_img(
-            os.path.join(
-                app.config['UPLOAD_PATH'], images.filename), target_size=(224, 224))
+            path, target_size=(224, 224))
         x = tf.keras.preprocessing.image.img_to_array(img)
         x = np.expand_dims(x, axis=0)
 
@@ -117,17 +138,11 @@ def result_model():
             disease = 'Hispa' 
             
         # get pesticide recommend
-        # disease = disease
-        rec = recommend_pestisida(disease = 'Brown Spot')
+        rec = recommend_pestisida(disease)
 
-        # return json 
-        return jsonify(penyakit = disease, recommendations = {
-                'nama' : rec.nama.to_json(),
-                'tempat' : rec.tempat.to_json()
-            })
+        return jsonify(penyakit = disease, recommendations = rec)
 
         # return web
-        #return render_template('resultModel.html', training=str(classes), hasil=str(result), nama=disease,recommend=rec )
-
+        # return render_template('resultModel.html', training=str(classes), hasil=str(result), nama=disease,recommend=rec )
 if __name__ == '__main__':
     app.run(ssl_context='adhoc', host='0.0.0.0', port=8080, debug=True)
