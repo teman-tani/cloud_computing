da0afe6b4ecadcbc11e0acb1e7197cfb69388494
slstyrn21 <slstyrn21@gmail.com>
2022-06-11 16:33:01 +0800

    coba create app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import (Flask,  render_template, request,jsonify
 from keras.models import load_model
 import tensorflow as tf
 import numpy as np
-import os
+import os, logging
 import pickle
 import pandas as pd
 
@@ -14,6 +14,24 @@ app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 app.config['UPLOAD_PATH'] = 'uploads'
 
 model = load_model('model_ml/my_model.h5')
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__,instance_relative_config=True)
+    app.config.from_mapping(SECRET_KEY='dev')
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
 @app.route('/')
 def index():
